a400c26118815ccf2a4b03b8a6c77eef1356a8ff
holygrail929 <holygrail929@naver.com>
2017-06-29 15:22:41 +0900

    메인메뉴 제어를 위한 스크립트 추가

---
+++ js/webcafe.js
@@ -0,0 +1,22 @@
+// 서브메인메뉴 제어를 위한 스크립트
+$(function() {
+    $(".main-menu>li").hover(
+        function() {
+            $(this).children("ul").css("display", "block");
+        },
+        function() {
+            $(this).children("ul").css("display", "none");
+        }
+    );
+
+    $(".main-menu span").focusin(
+        function() {
+            $(this).siblings("ul").css("display", "block");
+        }
+    );
+    $(".main-menu ul li:last-child a").focusout(
+        function() {
+            $(this).parent().parent().css("display", "none");
+        }
+    );
+});
