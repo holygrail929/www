166dc9d6d85994a63360ca0a15b21a0f9c152585
holygrail929 <holygrail929@naver.com>
2017-07-06 16:42:21 +0900

    공지사항 및 자료실 디자인 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -19,4 +19,11 @@ $(function() {
             $(this).parent().parent().css("display", "none");
         }
     );
+
+    // 탭메뉴 제어를 위한 자바스크립트
+    $(".board h2").focus(
+        function() {
+            $(this).parent().addClass("act").siblings().removeClass("act");
+        }
+    );
 });
